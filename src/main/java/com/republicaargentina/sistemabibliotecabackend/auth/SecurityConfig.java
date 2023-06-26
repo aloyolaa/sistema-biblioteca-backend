e4a5892b79d0d63aa13b9100008b3349f198d033
Alan Loyola <alanloyola2002@gmail.com>
2023-06-26 12:51:22 -0500

    Pruebas - 02

--- src/main/java/com/republicaargentina/sistemabibliotecabackend/auth/SecurityConfig.java
+++ src/main/java/com/republicaargentina/sistemabibliotecabackend/auth/SecurityConfig.java
@@ -22,7 +22,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
